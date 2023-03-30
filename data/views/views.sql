dc02000a0a45b7f160e51d0d2bfbdf83b2490e64
AlexD38 <dartiguelongue.alexis@gmail.com>
2023-03-30 10:54:59 +0200

    added view for next 3 appointments + added view for quick overview

--- data/views/views.sql
+++ data/views/views.sql
@@ -1,37 +1,47 @@
 BEGIN;
 
--- Enterprise gets all infos
+--DROP View quick_view;
 
-CREATE OR REPLACE VIEW ENTERPRISE_INFOS 
+CREATE OR REPLACE VIEW QUICK_VIEW 
 	AS
 	SELECT
-	    enterprises.name AS name_of_enterprise,
-	    enterprises.logo AS my_logo,
-	    enterprises.description AS description_of_enterprise,
-	    appointments.day,
-	    appointments.time_of_day,
-	    appointments.length_of_appointment AS duration,
-	    appointments.payment_method AS paid_with,
-	    clients.firstname AS firstname,
-	    clients.lastname AS lastname,
-	    clients.mail client_mail,
-	    clients.tel client_tel,
-	    services.name AS service,
-	    services.price AS service_price,
-	    services.description AS service_description,
-	    offers.discount AS discount,
-	    offers.description AS offer_description,
-	    offers.sales_before_offer AS sales_before_offer_is_applied
-	FROM enterprises
-	    JOIN appointments ON enterprise_id = enterprises.id
-	    JOIN clients ON clients.enterprise_id = enterprises.id
-	    JOIN offers ON offers.enterprise_id = enterprises.id
-	    JOIN services ON services.enterprise_id = enterprises.id
-	WHERE enterprises.id =
-1; 
+	    DISTINCT users.user_name as username,
+	    enterprises.name as entreprise,
+	    array_agg(services.description) AS services,
+	    count(DISTINCT services.id) AS number_of_services,
+	    array_agg(DISTINCT appointments.day) AS dates_rdv,
+	    count(DISTINCT appointments.id) as number_of_rdv,
+	    array_agg(DISTINCT offers.description) as offers,
+	    count(DISTINCT offers.id) as number_of_offers,
+	    array_agg(DISTINCT clients.firstname) as clients,
+	    count(
+	        DISTINCT enterprises_got_clients.clients_id
+	    ) as number_of_clients
+	from users
+	    JOIN enterprises on enterprise_id = enterprises.id
+	    JOIN appointments on appointments.enterprise_id = enterprises.id
+	    JOIN services on services.enterprise_id = enterprises.id
+	    JOIN offers on offers.enterprise_id = enterprises.id
+	    JOIN enterprises_got_clients on enterprises_got_clients.enterprises_id = enterprises.id
+	    JOIN clients on enterprises_got_clients.clients_id = clients.id
+	WHERE users.id = 1
+	GROUP BY
+	    users.user_name,
+	    enterprises.name
+; 
+
+--SELECT * from QUICK_VIEW;
+
+CREATE OR REPLACE VIEW NEXT_3_APPOINTMENTS 
+	AS
+	SELECT *
+	from appointments
+	WHERE day >= CURRENT_DATE
+	LIMIT 3
+; 
 
 ;
 
---Client gets all infos
+--SELECT * from next_3_appointments;
 
 COMMIT;
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,8 +14,8 @@ psql -f ./data/tables.sql
 psql -f  ./data/data.sql
 
 #functions
-psql -f ./data/functions/verify_clientid_before_update_appointment.sql
+#psql -f ./data/functions/verify_clientid_before_update_appointment.sql
 #psql -f ./data/functions/insert_user.sql
 
 #views
-# psql -f ./data/views/views.sql
+ psql -f ./data/views/views.sql

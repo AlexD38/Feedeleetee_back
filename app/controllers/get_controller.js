f95b478afac724ef8e5eda1824e281348606d1e4
Alexis Dartiguelongue <alexisdartiguelongue@Oming.local>
2023-06-13 23:28:31 +0200

    fixed need to re log to crud the api :)

--- app/controllers/get_controller.js
+++ app/controllers/get_controller.js
@@ -1,197 +1,198 @@
 import dataMapper from "../models/get_model.js";
 
 const get_controller = {
-	// changer await datamapper.blabla par getInfos
-	async clientInformation(req, res) {
-		try {
-			const clientId = res.locals.user.clientId;
-			if (!clientId) {
-				res.json({ failed: "client id is null" });
-				return;
-			}
-			let client = await dataMapper.getOneClient(clientId);
-			if (client) {
-				res.json(client);
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async clientsAppointments(req, res) {
-		try {
-			const clientId = req.params.id;
-			let clientsAppointments = await dataMapper.getOneClientAppointments(
-				clientId
-			);
-			if (clientsAppointments) {
-				res.json(clientsAppointments);
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async enterpriseClients(req, res) {
-		try {
-			const enterpriseId = res.locals.user.enterpriseId;
-			let enterpriseClients = await dataMapper.getclientsFromEnterprise(
-				enterpriseId
-			);
-			if (enterpriseClients) {
-				res.json(enterpriseClients);
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-		const enterpriseId = res.locals.user.enterpriseId;
-	},
-	async getAllInfosForMyEnterprise(req, res) {
-		try {
-			const enterpriseId = res.locals.user.enterpriseId;
-			const userId = res.locals.user.userId;
-			console.log(enterpriseId, userId);
-			let getAllInfosForMyEnterprise =
-				await dataMapper.getAllInfosForMyEnterprise(
-					userId,
-					enterpriseId
-				);
-			if (getAllInfosForMyEnterprise) {
-				// console.log(getAllInfosForMyEnterprise);
-				res.json(getAllInfosForMyEnterprise);
-			}
-			console.log("session : ", res.locals.user);
-		} catch (error) {
-			res.status(500).send({
-				message: "I can't give you that information right now...",
-				statusCode: 500,
-			});
-			console.log(error);
-		}
-	},
-	async enterpriseInformation(req, res) {
-		try {
-			const id = req.params.id;
-			let enterprises = await dataMapper.getEnterprises();
-			if (enterprises) {
-				res.json({
-					enterprises,
-				});
-			} else {
-				console.log("not ok");
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async oneEnterpriseInformation(req, res) {
-		const enterpriseId = res.locals.user.enterpriseId;
-		try {
-			let enterprise = await dataMapper.getOneEnterprise(enterpriseId);
-			if (enterprise) {
-				res.json(enterprise);
-			} else {
-				console.log("no enterprise found");
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async serviceInformationFromEnterprise(req, res) {
-		try {
-			const enterpriseId = res.locals.user.enterpriseId;
-			let services = await dataMapper.getServicesFromEnterprise(
-				enterpriseId
-			);
-			if (services) {
-				res.json(services);
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async appointmentInformationFromEnterprise(req, res) {
-		try {
-			const enterpriseId = res.locals.user.enterpriseId;
-			let appointments = await dataMapper.getAppointmentsFromEnterprise(
-				enterpriseId
-			);
-			if (appointments) {
-				res.json(appointments);
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async appointmentsAvailableFromEnterprise(req, res) {
-		try {
-			const enterpriseId = req.params.id;
-			console.log(req.params.id);
-			// console.log("ici");
-			let appointments =
-				await dataMapper.getAppointmentsAvailableFromEnterprise(
-					enterpriseId
-				);
-			if (appointments) {
-				res.json(appointments);
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async offerInformationFromEnterprise(req, res) {
-		try {
-			const enterpriseId = res.locals.user.enterpriseId;
-			let offers = await dataMapper.getOffersFromEnterprises(
-				enterpriseId
-			);
-			if (offers) {
-				res.json(offers);
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	// async appointmentInformation(req, res) {
-	// 	try {
-	// 		let offers = await dataMapper.getAppointments();
-	// 		if (offers) {
-	// 			res.json(offers);
-	// 		} else {
-	// 			console.log(error);
-	// 		}
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	}
-	// },
-	async getNextAppointments(req, res) {
-		try {
-			const enterpriseId = res.locals.user.enterpriseId;
+    // changer await datamapper.blabla par getInfos
+    async clientInformation(req, res) {
+        try {
+            const clientId = res.locals.user.clientId;
+            if (!clientId) {
+                res.json({ failed: "client id is null" });
+                return;
+            }
+            let client = await dataMapper.getOneClient(clientId);
+            if (client) {
+                res.json(client);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async clientsAppointments(req, res) {
+        try {
+            const clientId = req.params.id;
+            let clientsAppointments = await dataMapper.getOneClientAppointments(
+                clientId
+            );
+            if (clientsAppointments) {
+                res.json(clientsAppointments);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async enterpriseClients(req, res) {
+        try {
+            const enterpriseId = req.headers.enterpriseid;
+            let enterpriseClients = await dataMapper.getclientsFromEnterprise(
+                enterpriseId
+            );
+            if (enterpriseClients) {
+                res.json(enterpriseClients);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        const enterpriseId = res.locals.user.enterpriseId;
+    },
+    async getAllInfosForMyEnterprise(req, res) {
+        try {
+            const enterpriseId = res.locals.user.enterpriseId;
+            const userId = res.locals.user.userId;
+            console.log(enterpriseId, userId);
+            let getAllInfosForMyEnterprise =
+                await dataMapper.getAllInfosForMyEnterprise(
+                    userId,
+                    enterpriseId
+                );
+            if (getAllInfosForMyEnterprise) {
+                // console.log(getAllInfosForMyEnterprise);
+                res.json(getAllInfosForMyEnterprise);
+            }
+            console.log("session : ", res.locals.user);
+        } catch (error) {
+            res.status(500).send({
+                message: "I can't give you that information right now...",
+                statusCode: 500,
+            });
+            console.log(error);
+        }
+    },
+    async enterpriseInformation(req, res) {
+        try {
+            const id = req.params.id;
+            let enterprises = await dataMapper.getEnterprises();
+            if (enterprises) {
+                res.json({
+                    enterprises,
+                });
+            } else {
+                console.log("not ok");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async oneEnterpriseInformation(req, res) {
+        const enterpriseId = res.locals.user.enterpriseId;
+        try {
+            let enterprise = await dataMapper.getOneEnterprise(enterpriseId);
+            if (enterprise) {
+                res.json(enterprise);
+            } else {
+                console.log("no enterprise found");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async serviceInformationFromEnterprise(req, res) {
+        try {
+            const enterpriseId = req.headers.enterpriseid;
+            let services = await dataMapper.getServicesFromEnterprise(
+                enterpriseId
+            );
+            if (services) {
+                res.json(services);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async appointmentInformationFromEnterprise(req, res) {
+        try {
+            const enterpriseId = req.headers.enterpriseid;
+            console.log("chekc my appts", req.headers);
+            let appointments = await dataMapper.getAppointmentsFromEnterprise(
+                enterpriseId
+            );
+            if (appointments) {
+                res.json(appointments);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async appointmentsAvailableFromEnterprise(req, res) {
+        try {
+            const enterpriseId = req.params.id;
+            console.log(req.params.id);
+            // console.log("ici");
+            let appointments =
+                await dataMapper.getAppointmentsAvailableFromEnterprise(
+                    enterpriseId
+                );
+            if (appointments) {
+                res.json(appointments);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async offerInformationFromEnterprise(req, res) {
+        try {
+            const enterpriseId = req.headers.enterpriseid;
+            let offers = await dataMapper.getOffersFromEnterprises(
+                enterpriseId
+            );
+            if (offers) {
+                res.json(offers);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    // async appointmentInformation(req, res) {
+    // 	try {
+    // 		let offers = await dataMapper.getAppointments();
+    // 		if (offers) {
+    // 			res.json(offers);
+    // 		} else {
+    // 			console.log(error);
+    // 		}
+    // 	} catch (error) {
+    // 		console.log(error);
+    // 	}
+    // },
+    async getNextAppointments(req, res) {
+        try {
+            const enterpriseId = res.locals.user.enterpriseId;
 
-			let nextAppointments = await dataMapper.getNextAppointments(
-				enterpriseId
-			);
-			if (nextAppointments) {
-				res.json(nextAppointments);
-			} else {
-				console.log(error);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
+            let nextAppointments = await dataMapper.getNextAppointments(
+                enterpriseId
+            );
+            if (nextAppointments) {
+                res.json(nextAppointments);
+            } else {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
 };
 
 export default get_controller;
--- app/controllers/post_controller.js
+++ app/controllers/post_controller.js
@@ -3,167 +3,167 @@ import post_model from "../models/post_model.js";
 import multer from "multer";
 
 const post_controller = {
-	async createClient(req, res, next) {
-		try {
-			const { firstname, lastname, mail, tel } = req.body;
-			console.log(req.body);
+    async createClient(req, res, next) {
+        try {
+            const { firstname, lastname, mail, tel } = req.body;
+            console.log(req.body);
 
-			const results = await post_model.insertClient(
-				firstname,
-				lastname,
-				mail,
-				tel
-			);
-			res.locals.clientCreated = results;
-			console.log("client has been created : ", res.locals.clientCreated);
-			next();
-		} catch (error) {
-			console.log(error);
-		}
-	},
+            const results = await post_model.insertClient(
+                firstname,
+                lastname,
+                mail,
+                tel
+            );
+            res.locals.clientCreated = results;
+            console.log("client has been created : ", res.locals.clientCreated);
+            next();
+        } catch (error) {
+            console.log(error);
+        }
+    },
 
-	async createEnterprise(req, res, next) {
-		try {
-			const {
-				enterpriseName,
-				enterpriseAddress,
-				enterpriseDesc,
-				enterpriseLogo,
-			} = req.body;
+    async createEnterprise(req, res, next) {
+        try {
+            const {
+                enterpriseName,
+                enterpriseAddress,
+                enterpriseDesc,
+                enterpriseLogo,
+            } = req.body;
 
-			const results = await post_model.insertEnterprise(
-				enterpriseName,
-				enterpriseAddress,
-				enterpriseLogo,
-				enterpriseDesc
-			);
-			res.locals.enterpriseCreated = results;
+            const results = await post_model.insertEnterprise(
+                enterpriseName,
+                enterpriseAddress,
+                enterpriseLogo,
+                enterpriseDesc
+            );
+            res.locals.enterpriseCreated = results;
+            next();
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    },
 
-			next();
-		} catch (error) {
-			res.status(500).send(error);
-		}
-	},
+    async attachEnterpriseToUser(req, res) {
+        try {
+            console.log(
+                "enterpriseCreated from locals",
+                res.locals.enterpriseCreated
+            );
+            console.log("locals user from locals", res.locals.user);
+            const userId = res.locals.user.userId;
+            const enterpriseId = res.locals.enterpriseCreated.enterpriseId;
+            res.locals.user.enterpriseId =
+                res.locals.enterpriseCreated.enterpriseId;
 
-	async attachEnterpriseToUser(req, res) {
-		try {
-			console.log(
-				"enterpriseCreated from locals",
-				res.locals.enterpriseCreated
-			);
-			console.log("locals user from locals", res.locals.user);
-			const userId = res.locals.user.userId;
-			const enterpriseId = res.locals.enterpriseCreated.enterpriseId;
-			res.locals.user.enterpriseId =
-				res.locals.enterpriseCreated.enterpriseId;
+            console.log("user locals from locals 2", res.locals.user);
+            if (userId) {
+                const userCreateEnterprise =
+                    await post_model.insertEnterpriseIdIntoUserTable(
+                        enterpriseId,
+                        userId
+                    );
+                res.json({ userCreateEnterprise, enterpriseId, success: true });
+            } else {
+                res.json({
+                    error: "Something went wrong here...",
+                    success: false,
+                });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async attachClientToUser(req, res) {
+        try {
+            console.log(res.locals.clientCreated);
+            const userId = res.locals.user.userId;
+            const clientId = res.locals.clientCreated?.id;
+            // res.locals.user.clientId = clientId;
+            if (userId) {
+                const userCreateClient =
+                    await post_model.insertClientIdIntoUserTable(
+                        userId,
+                        clientId
+                    );
+                res.json({
+                    success: `client ${clientId} has been attached to user : ${userId}`,
+                    userCreateClient,
+                });
+            } else {
+                res.json({ error: "You gotta be connected to do this..." });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async createAppointments(req, res) {
+        const { day, timeOfDay, serviceId } = req.body.data;
+        console.log(req.body.data);
+        console.log(req.headers);
+        const enterpriseId = req.headers.enterpriseid;
+        // console.log(enterpriseId);
 
-			console.log("user locals from locals 2", res.locals.user);
-			if (userId) {
-				const userCreateEnterprise =
-					await post_model.insertEnterpriseIdIntoUserTable(
-						enterpriseId,
-						userId
-					);
-				res.json({ userCreateEnterprise, enterpriseId, success: true });
-			} else {
-				res.json({
-					error: "Something went wrong here...",
-					success: false,
-				});
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async attachClientToUser(req, res) {
-		try {
-			console.log(res.locals.clientCreated);
-			const userId = res.locals.user.userId;
-			const clientId = res.locals.clientCreated?.id;
-			// res.locals.user.clientId = clientId;
-			if (userId) {
-				const userCreateClient =
-					await post_model.insertClientIdIntoUserTable(
-						userId,
-						clientId
-					);
-				res.json({
-					success: `client ${clientId} has been attached to user : ${userId}`,
-					userCreateClient,
-				});
-			} else {
-				res.json({ error: "You gotta be connected to do this..." });
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	},
-	async createAppointments(req, res) {
-		const { day, timeOfDay, serviceId } = req.body.data;
-		console.log(req.body.data);
-		const enterpriseId = res.locals.user.enterpriseId;
-		// console.log(enterpriseId);
+        const results = await post_model.insertAppointements(
+            day,
+            timeOfDay,
+            serviceId,
+            +enterpriseId
+        );
+        res.json(results);
+    },
+    async createOffer(req, res) {
+        const { description, discount } = req.body.data;
+        const enterpriseId = req.headers.enterpriseid;
 
-		const results = await post_model.insertAppointements(
-			day,
-			timeOfDay,
-			serviceId,
-			enterpriseId
-		);
-		res.json(results);
-	},
-	async createOffer(req, res) {
-		const { description, discount } = req.body.data;
-		const enterpriseId = res.locals.user.enterpriseId;
+        const results = await post_model.insertOffers(
+            description,
+            discount,
+            enterpriseId
+        );
+        res.json(results);
+    },
+    async createService(req, res) {
+        const { description, price, duration } = req.body.data;
+        console.log(req.body);
+        const enterpriseId = req.headers.enterpriseid;
 
-		const results = await post_model.insertOffers(
-			description,
-			discount,
-			enterpriseId
-		);
-		res.json(results);
-	},
-	async createService(req, res) {
-		const { description, price, duration } = req.body.data;
-		console.log(req.body);
-		const enterpriseId = res.locals.user.enterpriseId;
+        const results = await post_model.insertServices(
+            description,
+            price,
+            duration,
+            enterpriseId
+        );
+        res.json(results);
+    },
+    async uploadLogo(req, res) {
+        try {
+            const imageData = req.file;
+            console.log(imageData); // récupère les données de l'image en bytea
+            const enterpriseId = req.headers.enterpriseid;
+            console.log(imageData);
+            console.log("enterpriseId: " + enterpriseId);
 
-		const results = await post_model.insertServices(
-			description,
-			price,
-			duration,
-			enterpriseId
-		);
-		res.json(results);
-	},
-	async uploadLogo(req, res) {
-		try {
-			const imageData = req.file;
-			console.log(imageData); // récupère les données de l'image en bytea
-			const enterpriseId = res.locals.user.enterpriseId;
-			console.log(imageData);
-			console.log("enterpriseId: " + enterpriseId);
+            const imageToUpload = req.file.buffer;
+            // console.log("buffer : ", imageToUpload);
 
-			const imageToUpload = req.file.buffer;
-			// console.log("buffer : ", imageToUpload);
-
-			const result = await post_model.insertLogo(
-				imageToUpload,
-				enterpriseId
-			);
-			if (result) {
-				res.status(200).json({
-					mesage: `Image téléchargée avec succès. Pour l'entreprise : ${result.name}`,
-					image: imageToUpload.toString("base64").toString("utf8"),
-				});
-			}
-		} catch (error) {
-			console.error(error);
-			res.status(500).send(
-				"Une erreur est survenue lors du téléchargement de l'image."
-			);
-		}
-	},
+            const result = await post_model.insertLogo(
+                imageToUpload,
+                enterpriseId
+            );
+            if (result) {
+                res.status(200).json({
+                    mesage: `Image téléchargée avec succès. Pour l'entreprise : ${result.name}`,
+                    image: imageToUpload.toString("base64").toString("utf8"),
+                });
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(
+                "Une erreur est survenue lors du téléchargement de l'image."
+            );
+        }
+    },
 };
 
 export default post_controller;

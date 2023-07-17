078519ed5aff1788e3241335be2025afce386e57
AlexD38 <dartiguelongue.alexis@gmail.com>
2023-07-17 16:32:05 +0200

    stil trying for vercel

--- api/index.js
+++ api/index.js
@@ -1,63 +1,55 @@
-// // import d'Express
-// import express from "express";
-// // création de server Express
-// const app = express();
-// import cors from "cors";
+// import d'Express
+import express from "express";
+// création de server Express
+const app = express();
+import cors from "cors";
 
-// // import du pkg, pour utiliser le __dirname
-// import { fileURLToPath } from "url";
-// import { dirname } from "path";
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+// import du pkg, pour utiliser le __dirname
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// // import de la session Express
-// import session from "express-session";
-// // import du router Express
-// import router from "../app/router.js";
-// // import du dotenv pour aller chercher les credentials de la db
-// import dotenv from "dotenv";
-// // config pour dotenv
-// dotenv.config();
+// import de la session Express
+import session from "express-session";
+// import du router Express
+import router from "../app/router.js";
+// import du dotenv pour aller chercher les credentials de la db
+import dotenv from "dotenv";
+// config pour dotenv
+dotenv.config();
 
-// // définition du port d'écoute
-// const PORT = process.env.PORT || 4000;
+// définition du port d'écoute
+const PORT = process.env.PORT || 4000;
 
-// // définition du titre du server Express
-// app.locals.siteName = "Feedeleetee";
+// définition du titre du server Express
+app.locals.siteName = "Feedeleetee";
 
-// // urlencoded pour pouvoir envoyer des données du front via requête POST
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+// urlencoded pour pouvoir envoyer des données du front via requête POST
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// // allow cors for everyone
-// app.use(cors());
+// allow cors for everyone
+app.use(cors());
 
-// // Express utilise la session et ses options
-// app.use(
-// 	session({
-// 		secret: process.env.SESSION_SECRET || "secret",
-// 		resave: false,
-// 		saveUninitialized: true,
-// 		cookie: { secure: false },
-// 	})
-// );
+// Express utilise la session et ses options
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET || "secret",
+		resave: false,
+		saveUninitialized: true,
+		cookie: { secure: false },
+	})
+);
 
-// // dfinition du chemin pour fichiers statiques
-// app.use(express.static(__dirname + "/public"));
+// dfinition du chemin pour fichiers statiques
+app.use(express.static(__dirname + "/public"));
 
-// // utilisation du mw d'auth ici
-// // app.use(signController.verifyUser);
-// //! authMiddleware.checkIfLogged
-// // utilisation du router
-// app.use(router);
-
-// // Ecoute du port + console du port d'écoute.
-// app.listen(PORT, () => {
-// 	console.log(`server running on http://localhost:${PORT}`);
-// });
-
-const app = require("express")();
-const { v4 } = require("uuid");
+// utilisation du mw d'auth ici
+// app.use(signController.verifyUser);
+//! authMiddleware.checkIfLogged
+// utilisation du router
+app.use(router);
 
 app.get("/api", (req, res) => {
 	const path = `/api/item/${v4()}`;
@@ -71,4 +63,7 @@ app.get("/api/item/:slug", (req, res) => {
 	res.end(`Item: ${slug}`);
 });
 
-module.exports = app;
+// Ecoute du port + console du port d'écoute.
+app.listen(PORT, () => {
+	console.log(`server running on http://localhost:${PORT}`);
+});

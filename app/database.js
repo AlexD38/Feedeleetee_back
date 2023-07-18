54473f8c48111ebd7aaac60f4e7925adfe42b294
AlexD38 <dartiguelongue.alexis@gmail.com>
2023-07-18 18:28:12 +0200

    added database and user into databa.js

--- app/database.js
+++ app/database.js
@@ -5,6 +5,8 @@ const { Pool } = pg;
 
 const pool = new Pool({
 	connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+	user: process.env.POSTGRES_USER,
+	database: process.env.POSTGRES_DATABASE,
 });
 
 console.log("database: " + pool.database, "User: " + pool.user);
